6c63adfa83b11e589d32fb2b2df127eae5cc6e7f
kioko <kiokotomas@gmail.com>
2015-12-31 15:38:56 +0300

    Created Model class

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Kioko on 31/12/2015.
+//  Copyright © 2015 Thomas Kioko. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
